e17ec897698127346e3907cd2224603e11a282c6
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-03 19:13:02 +0500

    code updated

--- app.js
+++ app.js
@@ -7,24 +7,48 @@ var getHours = document.getElementById("hr");
 var getMinutes = document.getElementById("min");
 var getSeconds = document.getElementById("sec");
 var getMilisec = document.getElementById("mil");
+var interval;
 
-setInterval( function(){
+var startBtn = document.querySelector(".startbtn");
+var stopBtn = document.querySelector(".stopbtn");
+var resetBtn = document.querySelector(".resetbtn");
+
+function start() {
+  startBtn.classList.add("active");
+  stopBtn.classList.remove("activeRemove");
+  interval = setInterval(function () {
     milisec++;
     getMilisec.innerHTML = milisec;
-    if(milisec >= 100){
-        sec++;
-        getSeconds.innerHTML = sec;
-        milisec = 0
+    if (milisec >= 100) {
+      sec++;
+      getSeconds.innerHTML = sec;
+      milisec = 0;
+    } else if (sec >= 5) {
+      min++;
+      getMinutes.innerHTML = min;
+      sec = 0;
+    } else if (min >= 2) {
+      hour++;
+      getHours.innerHTML = hour;
+      min = 0;
     }
-    else if(sec >=5){
-        min++;
-        getMinutes.innerHTML = min;
-        sec = 0
-    }
-    else if(min >= 2){
-hour++;
-getHours.innerHTML = hour;
-min = 0
-    }
-    
-}, 10)
+  }, 10);
+}
+
+function stop() {
+  clearInterval(interval);
+  startBtn.classList.remove("active");
+  stopBtn.classList.add("activeRemove");
+}
+
+function reset() {
+//   startBtn.classList.remove("active");
+  hour = 0;
+  min = 0;
+  sec = 0;
+  milisec = 0;
+  getHours.innerHTML = hour + "0";
+  getMinutes.innerHTML = min + "0";
+  getSeconds.innerHTML = sec + "0";
+  getMilisec.innerHTML = milisec + "0";
+}
